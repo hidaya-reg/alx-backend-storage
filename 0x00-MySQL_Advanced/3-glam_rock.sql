5f15b5f33ecb3ffb6f90c1fc3fa17f1b4602e85c
hidaya <hidaya.regragui@gmail.com>
2024-08-24 15:12:59 +0000

    task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- list all bands with Glam rock as main style, ranked by longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;

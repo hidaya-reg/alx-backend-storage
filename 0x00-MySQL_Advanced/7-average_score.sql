66ae2a9cd447de624709a95a5701317f01dcdd21
hidaya <hidaya.regragui@gmail.com>
2024-08-24 21:05:06 +0000

    task7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,7 @@
 -- Procedure that computes and store the average score for a student
 DELIMITER $$
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	DECLARE avg_score FLOAT;
@@ -12,3 +13,4 @@ BEGIN
 	SET average_score = avg_score
 	WHERE id = user_id;
 END $$
+DELIMITER ;

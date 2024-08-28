8dbe647861917bb8de69d5457c0933f123177c1b
hidaya <hidaya.regragui@gmail.com>
2024-08-28 21:17:19 +0100

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Redis client module
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -19,3 +19,20 @@ class Cache:
 		key = str(uuid.uuid4())
 		self._redis.set(key, data)
 		return key
+
+	def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+		"""Retrieve data from Redis and optionally apply a conversion fct"""
+		value = self._redis.get(key)
+		if value is None:
+			return None
+		if fn:
+			return fn(value)
+		return value
+
+	def get_str(self, key: str) -> Optional[str]:
+		"""Retrieve data from Redis and decode it as UTF-8 string"""
+		return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+	def get_int(self, key: str) -> Optional[int]:
+		"""Retrieve data from Redis and convert it to an integer"""
+		return self.get(key, fn=int)
